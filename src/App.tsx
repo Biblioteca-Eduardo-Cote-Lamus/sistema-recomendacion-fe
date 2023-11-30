3f106f9e9c93a5f6d2bd5e2074276029a5f99fdd
angedev07 <angelgaran20@gmail.com>
2023-11-30 13:22:46 -0500

    feat: Se implementa lo siguiente
    - Componentes para mostrar la informacion del usuario y los libros
    - Custom hook para el fetch de los datos

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,47 @@
-import { Box, Button } from "@mui/material";
+import { useState } from "react";
+import { Box, Button, Grid, Typography } from "@mui/material";
 import { useRef } from "react";
 import Webcam from "react-webcam";
+import { CardId, User } from "./components/CardId";
+import { useBooks } from "./hooks/useBooks";
+import { BookCard } from "./components/BookCard";
 
 export const App = () => {
   const camRef = useRef(null);
+  const { user, books, loading, fetchBooks } = useBooks();
 
   const capture = () => {
     const imageSrc = camRef.current.getScreenshot();
 
-    fetch("http://127.0.0.1:8000/recognition", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ img: imageSrc.split(',')[1] }),
-    })
-      .then((res) => res.json())
-      .then((res) => console.log(res))
-      .catch((error) => {
-        console.error(error);
-      });
+    fetchBooks(imageSrc);
   };
 
   return (
-    <Box sx={{ p: 2 }} component="section">
-      <Webcam
-        height={300}
-        width={300}
-        screenshotFormat="image/jpeg"
-        ref={camRef}
-      />
-      <Button variant="contained" onClick={capture}>
-        Tomar foto
-      </Button>
+    <Box component="section"  sx={{width: '90%', maxWidth: '1200px', margin:'auto'}}>
+      <Grid container component="section" spacing={2} >
+        <Grid item xs={12} md={6}>
+          <Box
+            component="div"
+            sx={{ mb: 2 }}
+            display="flex"
+            flexDirection="column"
+          >
+            <Webcam screenshotFormat="image/jpeg" ref={camRef} />
+            <Button variant="contained" onClick={capture} sx={{ marginTop: 2 }}>
+              Tomar foto
+            </Button>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <CardId user={user!} loading={loading} />
+        </Grid>
+      </Grid>
+
+      <Typography variant="h4" component="h1" align="center" sx={{ margin: 2 }}>
+        {" "}
+        Listado de Libros{" "}
+      </Typography>    
+        <BookCard books={books} />
     </Box>
   );
 };
